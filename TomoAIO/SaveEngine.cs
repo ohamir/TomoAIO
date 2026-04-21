9055249cb006ac2ea7111a41322439ef22a3d940
GingnamFong <90309841+GingnamFong@users.noreply.github.com>
2026-04-21 17:07:36 +0200

    Revert "Refactored the project toward OOP by splitting core logic"
    This reverts commit e275ad27ce3aa6b9486f9688574f1ee959303ffd.

--- TomoAIO/SaveEngine.cs
+++ TomoAIO/SaveEngine.cs
@@ -1,28 +1,34 @@
-﻿namespace TomoAIO
+﻿using System;
+
+public class SaveEngine
 {
-    public static class SaveEngine
+    // This is our C# version of the offsetLocator!
+    public static int GetDataOffset(byte[] saveFileBytes, string hashStr)
     {
-        public static int GetDataOffset(byte[] saveFileBytes, string hashStr)
+        // 1. Convert the hash string (like "2499BFDA") into bytes
+        byte[] hashBytes = new byte[hashStr.Length / 2];
+        for (int i = 0; i < hashBytes.Length; i++)
         {
-            byte[] hashBytes = new byte[hashStr.Length / 2];
-            for (int i = 0; i < hashBytes.Length; i++)
-            {
-                hashBytes[i] = Convert.ToByte(hashStr.Substring(i * 2, 2), 16);
-            }
-
-            Array.Reverse(hashBytes);
-            for (int i = 0; i < saveFileBytes.Length - 8; i++)
-            {
-                if (saveFileBytes[i] == hashBytes[0] &&
-                    saveFileBytes[i + 1] == hashBytes[1] &&
-                    saveFileBytes[i + 2] == hashBytes[2] &&
-                    saveFileBytes[i + 3] == hashBytes[3])
-                {
-                    return BitConverter.ToInt32(saveFileBytes, i + 4);
-                }
-            }
-
-            return -1;
+            hashBytes[i] = Convert.ToByte(hashStr.Substring(i * 2, 2), 16);
         }
+
+        // 2. Reverse it because Switch uses "Little Endian" format
+        Array.Reverse(hashBytes);
+
+        // 3. Search the save file for this specific hash
+        for (int i = 0; i < saveFileBytes.Length - 8; i++)
+        {
+            if (saveFileBytes[i] == hashBytes[0] &&
+                saveFileBytes[i + 1] == hashBytes[1] &&
+                saveFileBytes[i + 2] == hashBytes[2] &&
+                saveFileBytes[i + 3] == hashBytes[3])
+            {
+                // 4. We found the hash! The actual offset is the next 4 bytes.
+                return BitConverter.ToInt32(saveFileBytes, i + 4);
+            }
+        }
+
+        // Return -1 if something went wrong and we couldn't find it
+        return -1;
     }
 }
