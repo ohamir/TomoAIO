d01e8fdd44958b440c1b01c10c3cfe2faab99bab
GingnamFong <90309841+GingnamFong@users.noreply.github.com>
2026-04-23 20:57:50 +0200

    coments

--- TomoAIO/SaveEngine.cs
+++ TomoAIO/SaveEngine.cs
@@ -2,20 +2,18 @@
 
 public class SaveEngine
 {
-    // This is our C# version of the offsetLocator!
     public static int GetDataOffset(byte[] saveFileBytes, string hashStr)
     {
-        // 1. Convert the hash string (like "2499BFDA") into bytes
+        // Parse to bytes.
         byte[] hashBytes = new byte[hashStr.Length / 2];
         for (int i = 0; i < hashBytes.Length; i++)
         {
             hashBytes[i] = Convert.ToByte(hashStr.Substring(i * 2, 2), 16);
         }
 
-        // 2. Reverse it because Switch uses "Little Endian" format
+        // Hashes are stored in the table.
         Array.Reverse(hashBytes);
 
-        // 3. Search the save file for this specific hash
         for (int i = 0; i < saveFileBytes.Length - 8; i++)
         {
             if (saveFileBytes[i] == hashBytes[0] &&
@@ -23,12 +21,11 @@ public class SaveEngine
                 saveFileBytes[i + 2] == hashBytes[2] &&
                 saveFileBytes[i + 3] == hashBytes[3])
             {
-                // 4. We found the hash! The actual offset is the next 4 bytes.
                 return BitConverter.ToInt32(saveFileBytes, i + 4);
             }
         }
 
-        // Return -1 if something went wrong and we couldn't find it
+        // Not found.
         return -1;
     }
 }
